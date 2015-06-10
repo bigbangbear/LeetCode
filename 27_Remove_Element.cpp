6e1ff687230cd4a2b95c42b5b6ef7c31299a2e36
yuhuibin123 <385169523@qq.com>
2015-06-10 23:24:27 +0800

    the solution of 27 ,add comment

--- 27_Remove_Element.cpp
+++ 27_Remove_Element.cpp
@@ -1,11 +1,11 @@
+//这段题目较为简单，是将一个数组里剔除关键数字，并且返回最后的长度，算法思想，找到关键数字后将后面的数字往前移动一位，长度减一即可
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int n = nums.size();
         
         for(int i=0; i<n;){
-            int tmp = nums[i];
-            if(tmp == val){
+            if(nums[i] == val){
                 for(int j=i+1; j<n; j++){
                     nums[j-1] = nums[j];
                 }
