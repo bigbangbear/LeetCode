dd5536a560aab4e37d0e0a3b7ad6674b496ddeee
hzyuhuibin <hzyuhuibin@corp.neteae.com>
2020-03-31 17:29:25 +0800

    add: happy hacking

--- 10-regular-expression-matching.java
+++ 10-regular-expression-matching.java
@@ -1,21 +1,45 @@
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 正则匹配，还是大佬牛逼呀
+ */
 class Solution {
+    Map<String, Boolean> dp = new HashMap<>();
     public boolean isMatch(String s, String p) {
-        if (p.length() == 0){
-            return s.length() == 0;
+        String key = getKey(s, p);
+        if (dp.containsKey(key)) {
+            return dp.get(key);
         }
-        char sChar = s.charAt(0);
-        char pChar = p.charAt(0);
-        if (sChar == pChar) {
-            return isMatch(s.substring(1), p.substring(1));
-        }else{
-            if (pChar == '.') {
-                return isMatch(s.substring(1), p.substring(1)) || isMatch(s.substring(0), p.substring(1));
-            }else if (pChar == '*') {
-                return isMatch()
+        if (p.isEmpty()) {
+            dp.put(key, s.isEmpty());
+            return s.isEmpty();
+        }
+        boolean firstMatch = false;
+        if (!s.isEmpty()){
+            firstMatch = (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        }
+        if (p.length() >= 2 && p.charAt(1) == '*') {
+            boolean match = isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
+            dp.put(key, match);
+            return match;
+        }
+        boolean match = firstMatch && isMatch(s.substring(1), p.substring(1));
+        dp.put(key, match);
+        return match;
+    }
 
-            }else {
-                return false;
-            }
-        }
+    private String getKey(String s, String p) {
+        return "s=" + s + "p=" + p;
+    }
+}
+
+class Main {
+
+    public static void main(String[] args) {
+        String s = "aab";
+        String p = ".*";
+        Solution solution = new Solution();
+        boolean match = solution.isMatch(s, p);
+        System.out.println(match);
     }
 }
