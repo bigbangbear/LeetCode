9b314d95652a5d25391f475ff45a1cebec4e9ae4
hzyuhuibin <hzyuhuibin@corp.neteae.com>
2020-08-04 10:09:17 +0800

    add: happy hacking

--- 986-interval-list-intersections.java
+++ 986-interval-list-intersections.java
@@ -4,70 +4,38 @@ import java.util.Collections;
 import java.util.Comparator;
 
 class Solution {
-    class Node{
-        int left;
-        int right;
-        int index;
-    }
     public int[][] intervalIntersection(int[][] A, int[][] B) {
         if (A.length == 0 || B.length == 0) {
-            return null;
+            return new int[0][0];
         }
-
-        List<Node> nodes = new ArrayList<Node>();
-        add(A, nodes, 1);
-        add(B, nodes, 2);
-
-        Collections.sort(nodes, new Comparator<Node>() {
-
-            @Override
-            public int compare(Node o1, Node o2) {
-                return o1.left - o2.left;
-            }
-        });
-
-        List<Node> result = new ArrayList<Node>();
-        for (int i = 0; i < nodes.size() - 1; i++){
-            Node node = nodes.get(i);
-            Node next = nodes.get(i + 1);
-
-            if (node.index == next.index) {
-                continue;
+        List<int[]> list= new ArrayList<>();
+        int i=0;
+        int j=0;
+        while (i < A.length && j < B.length) {
+            if (A[i][0] <= B[j][0]) {
+                if (A[i][1] >= B[j][0]) {
+                    add(list, B[j][0], Math.min(B[j][1], A[i][1]));
+                }
             }else {
-                if (node.right < next.left) {
-                    continue;
-                }
-                Node newNode = new Node();
-                newNode.left = next.left;
-                newNode.right = Math.min(next.right, node.right);
-                if (result.size() <= 0) {
-                    result.add(newNode);
-                }else {
-                    Node last = result.get(result.size() - 1);
-                    if (last.right >= newNode.left) {
-                        last.right = newNode.right;
-                    }else {
-                        result.add(newNode);
-                    }
+                if (B[j][1] >= A[i][0]) {
+                    add(list, A[i][0], Math.min(A[i][1], B[j][1]));
                 }
             }
+            if (A[i][1] <= B[j][1]) {
+                i++;
+            }else {
+                j++;
+            }
         }
-        int[][] out = new int[result.size()][2];
-        for (int i = 0; i < result.size(); i++){
-            out[i][0] = result.get(i).left;
-            out[i][1] = result.get(i).right;
-        }
-        return out;
+        return list.toArray(new int[list.size()][]);
     }
 
-    private void add(int[][] A, List<Node> nodes, int index) {
-        for (int i = 0; i < A.length; i++){
-            Node node = new Node();
-            node.left = A[i][0];
-            node.right = A[i][1];
-            node.index = index;
-            nodes.add(node);
-        }
+    public void add(List<int[]> list, int left, int right) {
+        int[] tmp = new int[2];
+        tmp[0] = left;
+        tmp[1] = right;
+        list.add(tmp);
+        System.out.println("left = " + left + ", right = " + right);
     }
 
     public static void main(String[] args)  {
