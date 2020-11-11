bbed5b2a508938ccf062f0277b0261b5d1899965
bear <15988112961@163.com>
2020-11-11 17:16:04 +0800

    add: happy hacking 2020

---
+++ 167.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int []result = new int[2];
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                result[0] =left+1;
+                result[1] =right+1;
+                return result;
+            }else if (sum < target) {
+                left ++;
+            }else {
+                right --;
+            }
+        }
+        return null;
+    }
+}
