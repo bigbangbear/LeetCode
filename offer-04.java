c5c5960c50999f2ab2157bd5668bfb69ee33f691
bear <15988112961@163.com>
2021-01-06 23:31:10 +0800

    add: happy hacking 2020

--- offer-04.java
+++ offer-04.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) {
@@ -26,5 +27,27 @@ class Solution {
         }
 
         return false;
+    }
+}
+
+class Solution {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int x=0;
+        int y=matrix[0].length-1;
+        while (x >= 0 && x<matrix.length && y >=0 && y<matrix[0].length) {
+            //System.out.println(matrix[x][y]);
+            if (matrix[x][y] == target) {
+                return true;
+            }else if (matrix[x][y] < target) {
+                x++;
+            }else {
+                y--;
+            }
+        }
+        return false;
+
     }
 }
