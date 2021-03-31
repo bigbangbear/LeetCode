71f29c408c189bab82a3b521954a15b75ec195ee
bear <15988112961@163.com>
2021-03-31 15:46:56 +0800

    add: 每天进步一点点, 2021 加油

--- 53-max-subarray.java
+++ 53-max-subarray.java
@@ -25,3 +25,82 @@ class Solution {
         System.out.println("max" + max);
     }
 }
+
+
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+//输出：6
+//解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0]
+//输出：0
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：nums = [-1]
+//输出：-1
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：nums = [-100000]
+//输出：-100000
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 104 
+// -105 <= nums[i] <= 105 
+// 
+//
+// 
+//
+// 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。 
+// Related Topics 数组 分治算法 动态规划 
+// 👍 3070 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = Integer.MIN_VALUE;
+        int pre = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (pre > 0) {
+                pre = pre + nums[i];
+            }else {
+                pre = nums[i];
+            }
+            max = Math.max(pre, max);
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
