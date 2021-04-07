2248af583290c6a73a6174abb4d69ae9210ac78b
bear <15988112961@163.com>
2021-04-07 11:04:20 +0800

    add: 每天进步一点点, 2021 加油

---
+++ 99-recover-binary-search-tree.java
@@ -0,0 +1,105 @@
+//给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。 
+//
+// 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,3,null,null,2]
+//输出：[3,1,null,null,2]
+//解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [3,1,4,null,null,2]
+//输出：[2,1,4,null,null,3]
+//解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树上节点的数目在范围 [2, 1000] 内 
+// -231 <= Node.val <= 231 - 1 
+// 
+// Related Topics 树 深度优先搜索 
+// 👍 445 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void recoverTree(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        scan(root, list);
+        List<Integer> index = new ArrayList<>();
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i).val > list.get(i + 1).val) {
+                index.add(i);
+            }
+        }
+        TreeNode f = null;
+        TreeNode s = null;
+        if (index.size() == 1) {
+            f = list.get(index.get(0));
+            s = list.get(index.get(0) + 1);
+        } else {
+            f = list.get(index.get(0));
+            s = list.get(index.get(1) + 1);
+        }
+        int tmp = f.val;
+        f.val = s.val;
+        s.val = tmp;
+
+    }
+
+    private void scan(TreeNode root, List<TreeNode> list) {
+        if (root == null) {
+            return;
+        }
+        scan(root.left, list);
+        list.add(root);
+        scan(root.right, list);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void recoverTree(TreeNode root) {
+
+    }
+}
