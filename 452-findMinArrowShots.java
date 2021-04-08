aa8b2a0481524de35c2f8ff67fc4b5e48b8f0510
bear <15988112961@163.com>
2021-04-08 11:16:46 +0800

    add: 每天进步一点点, 2021 加油

--- 452-findMinArrowShots.java
+++ 452-findMinArrowShots.java
@@ -93,3 +93,33 @@ class Solution {
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
+
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        if (points == null || points.length == 0) {
+            return 0;
+        }
+        Arrays.sort(points, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return 0;
+                }else if (o1[1] < o2[1]) {
+                    return -1;
+                }else{
+                    return 1;
+                }
+            }
+        });
+        int count = 1;
+        int end = points[0][1];
+        for (int i=1; i<points.length; i++) {
+            int[] point = points[i];
+            if (point[0] > end) {
+                count++;
+                end = point[1];
+            }
+        }
+        return count;
+    }
+}
